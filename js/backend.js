e98ec416a6554ebdeb9532283be951c870b18e4a
SnowballBoy <54330340+SnowballBoy@users.noreply.github.com>
2020-02-25 18:15:53 +0300

    backend

---
+++ js/backend.js
@@ -0,0 +1,57 @@
+'use strict';
+
+(function () {
+  var load = function (onLoad, onError) {
+    var URL = 'https://js.dump.academy/code-and-magick/data';
+    var xhr = new XMLHttpRequest();
+    xhr.responseType = 'json';
+
+    xhr.open('get', URL);
+    xhr.addEventListener('load', function () {
+      if (xhr.status === 200 && xhr.readyState === XMLHttpRequest.DONE) {
+        onLoad(xhr.response);
+      } else {
+        onError('Статус ответа: ' + xhr.status + ' ' + xhr.statusText);
+      }
+    });
+    xhr.addEventListener('error', function () {
+      onError('Произошла ошибка соединения.');
+    });
+    xhr.addEventListener('timeout', function () {
+      onError('Истекло время ожидания.');
+    });
+    xhr.timeout = 10000;
+
+    xhr.send();
+  };
+
+
+  var save = function (data, onLoad, onError) {
+    var URL = 'https://js.dump.academy/code-and-magick';
+    var xhr = new XMLHttpRequest();
+    xhr.responseType = 'json';
+
+    xhr.open('post', URL);
+    xhr.addEventListener('load', function () {
+      if (xhr.status === 200 && xhr.readyState === XMLHttpRequest.DONE) {
+        onLoad(xhr.response);
+      } else {
+        onError('Статус ответа: ' + xhr.status + ' ' + xhr.statusText);
+      }
+    });
+    xhr.addEventListener('error', function () {
+      onError('Произошла ошибка соединения.');
+    });
+    xhr.addEventListener('timeout', function () {
+      onError('Истекло время ожидания.');
+    });
+    xhr.timeout = 10000;
+    xhr.send(data);
+  };
+
+
+  window.backend = {
+    load: load,
+    save: save
+  };
+})();
